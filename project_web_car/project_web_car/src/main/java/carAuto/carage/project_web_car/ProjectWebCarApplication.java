6dabd46dd3af38f48eb10bb0b3137c8017876b5e
leangsengLy <edeepleang@gmail.com>
2025-12-03 14:35:40 +0700

    start project 2025-03-12

---
+++ project_web_car/project_web_car/src/main/java/carAuto/carage/project_web_car/ProjectWebCarApplication.java
@@ -0,0 +1,13 @@
+package carAuto.carage.project_web_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebCarApplication.class, args);
+	}
+
+}
---
+++ project_web_car/project_web_car/src/test/java/carAuto/carage/project_web_car/ProjectWebCarApplicationTests.java
@@ -0,0 +1,13 @@
+package carAuto.carage.project_web_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
